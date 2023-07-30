feecd3ff85f568a406b49df331410ff0d454ce4e
c.bushehry <c.bushehry@gmail.com>
2023-07-30 16:01:13 -0700

    added event listeners to q1 v1

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,37 @@ let question = {
   
   // function for showing the question
   function showQuestion(q) {
-    // 1. select dom element
-    let titleDiv = document.getElementById("title");
-  
-    // 2. modify it
+    // show question title
+    let titleDiv = document.getElementById('title');
     titleDiv.textContent = q.title;
     
-    // selecting by a query
+    // show alternatives
     let alts = document.querySelectorAll('.alternative');
-    console.log(alts);
     
-    // show each alternative
     alts.forEach(function(element, index){
       element.textContent = q.alternatives[index];
+      
+      // listening for events
+      element.addEventListener('click', function(){
+        // check correct answer
+        if(q.correctAnswer == index) {
+          console.log('Correct Answer!');
+        }
+        else {
+          console.log('Wrong Answer!');
+        }
+      });
+      
     });
   }
   
   // call the function
   showQuestion(question);
+  
+  // select the button element
+  let btn = document.getElementById('btn');
+  
+  // listening to click events
+  btn.addEventListener('click', function(){
+    console.log('Clicked!');
+  });
