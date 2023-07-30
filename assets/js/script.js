af11c25f7920b0ec1a5c45d2f9e2ebf865e3e3ab
c.bushehry <c.bushehry@gmail.com>
2023-07-30 16:10:36 -0700

    refactored script.js v3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,41 @@
 // define the object for the question entity
+// define the object for the question entity
 let question = {
     title: 'gato',
     alternatives: ['dog', 'cat', 'bird', 'fish'],
     correctAnswer: 1
   };
   
-  // function for starting the application
-  function start() {
-    // get alternatives
-    let alts = document.querySelectorAll('.alternative');
+  let app = {
+    start: function() {
+      // get alternatives
+      let alts = document.querySelectorAll('.alternative');
     
-    alts.forEach(function(element, index){
-      element.addEventListener('click', function(){
-        // check correct answer
-        console.log('check correct answer');
+      alts.forEach(function(element, index){
+        element.addEventListener('click', function(){
+          // check correct answer
+          console.log('check correct answer');
+        });
       });
-    });
     
-    // show first question
-    showQuestion(question);
-  }
+      // show first question
+      this.showQuestion(question);
+    },
+    
+    showQuestion: function(q) {
+      // show question title
+      let titleDiv = document.getElementById('title');
+      titleDiv.textContent = q.title; 
+    
+      // show alternatives
+      let alts = document.querySelectorAll('.alternative');
+    
+      alts.forEach(function(element, index){
+        element.textContent = q.alternatives[index];
+      });
+    }
+  };
   
-  // function for showing the question
-  function showQuestion(q) {
-    // show question title
-    let titleDiv = document.getElementById('title');
-    titleDiv.textContent = q.title; 
-    
-    // show alternatives
-    let alts = document.querySelectorAll('.alternative');
-    
-    alts.forEach(function(element, index){
-      element.textContent = q.alternatives[index];
-    });
-  }
-  
-  // call the function for starting the application
-  start();
+  // initialize the application
+  app.start();
   
