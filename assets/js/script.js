dc89fd780a4733ae3249378736ede96824af38ba
c.bushehry <c.bushehry@gmail.com>
2023-07-31 23:17:23 -0700

    script.js v3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,66 @@
 // define the object for the question entity
-// define the object for the question entity
 let question = {
-    title: 'gato',
-    alternatives: ['dog', 'cat', 'bird', 'fish'],
-    correctAnswer: 1
-  };
+  title: 'gato',
+  alternatives: ['dog', 'cat', 'bird', 'fish'],
+  correctAnswer: 1
+};
+
+let app = {
+  start: function() {
+    // get alternatives
+    let alts = document.querySelectorAll('.alternative');
   
-  let app = {
-    start: function() {
-      // get alternatives
-      let alts = document.querySelectorAll('.alternative');
+    // first method of passing context
+    // alts.forEach(function(element, index){
+    //         
+    //   element.addEventListener('click', function(){
+    //     // check correct answer
+    //     this.checkAnswer(index);
+    //   }.bind(this));
+    // }.bind(this));
     
-      alts.forEach(function(element, index){
-        element.addEventListener('click', function(){
-          // check correct answer
-          console.log('check correct answer');
-        });
+    // second method of passing context
+    alts.forEach((element, index) => {
+            
+      element.addEventListener('click', () => {
+        // check correct answer
+        this.checkAnswer(index);
       });
+    });
     
-      // show first question
-      this.showQuestion(question);
-    },
+    // show first question
+    this.showQuestion(question);
+  },
+  
+  showQuestion: function(q) {
     
-    showQuestion: function(q) {
-      // show question title
-      let titleDiv = document.getElementById('title');
-      titleDiv.textContent = q.title; 
+    // keep track of current question
+    this.currQuestion = q;
     
-      // show alternatives
-      let alts = document.querySelectorAll('.alternative');
-    
-      alts.forEach(function(element, index){
-        element.textContent = q.alternatives[index];
-      });
+    // show question title
+    let titleDiv = document.getElementById('title');
+    titleDiv.textContent = q.title; 
+  
+    // show alternatives
+    let alts = document.querySelectorAll('.alternative');
+  
+    alts.forEach(function(element, index){
+      element.textContent = q.alternatives[index];
+    });
+  },
+  
+  checkAnswer: function(userSelected) {
+    if(this.currQuestion.correctAnswer == userSelected) {
+      // correct
+      console.log('correct');
     }
-  };
+    else {
+      // not correct
+      console.log('wrong');
+    }
+  }
   
-  // initialize the application
-  app.start();
-  
+};
+
+// initialize the application
+app.start();
