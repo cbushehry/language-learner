1bdc63c26012baa3f76a45641a25dc38a837c56d
c.bushehry <c.bushehry@gmail.com>
2024-06-27 23:08:59 -0700

    added 18 more animal questions to learn

---
+++ assets/js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+      title: 'gato', // 1 cat
+      alternatives: ['dog', 'cat', 'bird', 'fish'],
+      correctAnswer: 1
+    },
+    {
+      title: 'ave', // 2 bird
+      alternatives: ['mouse', 'hamster', 'lizard', 'bird'],
+      correctAnswer: 3
+    },
+    {
+      title: 'rata', // 3 rat
+      alternatives: ['cat', 'fish', 'rat', 'shark'],
+      correctAnswer: 2
+    },
+    {
+      title: 'mosca', // 4 fly
+      alternatives: ['fly', 'puma', 'fish', 'dog'],
+      correctAnswer: 0
+    },
+    {
+      title: 'perro', // 5 dog
+      alternatives: ['cat', 'fish', 'dog', 'bird'],
+      correctAnswer: 2
+    },
+    {
+      title: 'pez', // 6 fish
+      alternatives: ['lizard', 'hamster', 'fish', 'shark'],
+      correctAnswer: 2
+    },
+    {
+      title: 'lagarto', // 7 lizard
+      alternatives: ['mouse', 'lizard', 'puma', 'fly'],
+      correctAnswer: 1
+    },
+    {
+      title: 'ratón', // 8 mouse
+      alternatives: ['hamster', 'shark', 'dog', 'mouse'],
+      correctAnswer: 3
+    },
+    {
+      title: 'tiburón', // 9 shark
+      alternatives: ['shark', 'lizard', 'bird', 'fly'],
+      correctAnswer: 0
+    },
+    {
+        title: 'conejo', // 10 rabbit
+        alternatives: ['rabbit', 'horse', 'elephant', 'giraffe'],
+        correctAnswer: 0
+    },
+    {
+        title: 'caballo', // 11 horse
+        alternatives: ['lion', 'horse', 'turtle', 'bear'],
+        correctAnswer: 1
+    },
+    {
+        title: 'jirafa', // 12 giraffe
+        alternatives: ['wolf', 'cow', 'giraffe', 'pig'],
+        correctAnswer: 2
+    },
+    {
+        title: 'oso', // 13 bear
+        alternatives: ['rabbit', 'bear', 'horse', 'elephant'],
+        correctAnswer: 1
+    },
+    {
+        title: 'lobo', // 14 wolf
+        alternatives: ['giraffe', 'lion', 'wolf', 'turtle'],
+        correctAnswer: 2
+    },
+    {
+        title: 'vaca', // 15 cow
+        alternatives: ['cow', 'pig', 'rabbit', 'horse'],
+        correctAnswer: 0
+    },
+    {
+        title: 'cerdo', // 16 pig
+        alternatives: ['bear', 'elephant', 'cow', 'pig'],
+        correctAnswer: 3
+    },
+    {
+        title: 'tortuga', // 17 turtle
+        alternatives: ['turtle', 'wolf', 'lion', 'rabbit'],
+        correctAnswer: 0
+    },
+    {
+        title: 'zorro', // 18 fox
+        alternatives: ['fox', 'duck', 'monkey', 'sheep'],
+        correctAnswer: 0
+      },
+      {
+        title: 'pato', // 19 duck
+        alternatives: ['chicken', 'monkey', 'duck', 'fox'],
+        correctAnswer: 2
+      },
+      {
+        title: 'mono', // 20 monkey
+        alternatives: ['sheep', 'monkey', 'duck', 'chicken'],
+        correctAnswer: 1
+      },
+      {
+        title: 'oveja', // 21 sheep
+        alternatives: ['duck', 'sheep', 'chicken', 'monkey'],
+        correctAnswer: 1
+      },
+      {
+        title: 'gallina', // 22 chicken
+        alternatives: ['fox', 'sheep', 'monkey', 'chicken'],
+        correctAnswer: 3
+      }
+  ];
